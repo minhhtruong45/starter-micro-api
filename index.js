eefe142bf440c04972d3533bceedc565e91be295
minhhtruong45 <142463236+minhhtruong45@users.noreply.github.com>
2023-08-18 00:17:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-var http = require('http');
+const http = require('http');
+const os = require('os');
+
 http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yozzzx!');
-    res.end();
+  console.log(`Just got a request at ${req.url}!`);
+
+  const osInfo = {
+    platform: os.platform(),
+    type: os.type(),
+    release: os.release(),
+    totalMemory: os.totalmem(),
+    freeMemory: os.freemem(),
+    uptime: os.uptime()
+  };
+
+  const cpuInfo = {
+    model: os.cpus()[0].model,
+    speed: os.cpus()[0].speed,
+    cores: os.cpus().length
+  };
+
+  const systemInfo = {
+    os: osInfo,
+    cpu: cpuInfo
+  };
+
+  res.setHeader('Content-Type', 'application/json');
+  res.write(JSON.stringify(systemInfo));
+  res.end();
 }).listen(process.env.PORT || 3000);
